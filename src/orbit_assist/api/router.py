7038296d277692d4dc80f2994980a7f187e2cb63
Aaron Wright <aaron@sikosoft.com>
2026-03-20 20:33:50 +0100

    move calendar to get_calendar_events

--- src/orbit_assist/api/router.py
+++ src/orbit_assist/api/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from orbit_assist.api.routes.entities import router as entities_router
-from orbit_assist.api.routes.calendar import router as calendar_router
+from orbit_assist.api.routes.get_calendar_events import router as calendar_router
 from orbit_assist.api.routes.health import router as health_router
 from orbit_assist.api.routes.jobs import router as jobs_router
 from orbit_assist.api.routes.prompt import router as prompt_router
